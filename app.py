7fe06a93e9af086dcf6cdc517d5c6e90b8e85a5d
Snehal25309 <kolekarsnehal1987@gmail.com>
2026-08-27 11:47:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def ask_cloud_ai(question):
         )
 
         response = client.chat.completions.create(
-            model="llama-3.3-70b-versatile",
+            model="openai/gpt-oss-20b",,
             messages=[
                 {
                     "role": "system",
